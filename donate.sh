9ad568b9a4d55949000883a428ef520224446c9a
Markus Bertheau <mbertheau@gmail.com>
2020-06-21 00:46:53 +0200

    More changes

--- donate.sh
+++ donate.sh
@@ -1,4 +1,4 @@
 cd
-tar cf /media/psf/Home/donation.tar .ssh/id_rsa .ssh/id_rsa.pub
+tar cf /media/psf/Home/donation.tar .ssh/id_rsa .ssh/id_rsa.pub .ssh/config .password-store .gnupg
 read -p "Donating... Press Enter when finished."
 rm /media/psf/Home/donation.tar
