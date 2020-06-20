633932d83a9b17a2dc7b4d170373b81644a19b70
Markus Bertheau <mbertheau@gmail.com>
2020-06-20 21:09:07 +0200

    Move to donations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,9 @@
 # wget -q https://raw.githubusercontent.com/mbertheau/dotfiles/new-master/bootstrap.sh
 # set up ssh
 
-mkdir --mode=700 ~/.ssh
-
-touch ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-
-echo "Enter ~/.ssh/id_rsa"
-cat > ~/.ssh/id_rsa
-
-touch ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa.pub
-
-echo "Enter ~/.ssh/id_rsa.pub"
-cat > ~/.ssh/id_rsa.pub
+read -p "Make sure the donation is ready, then press Enter."
+tar xf /media/psf/Home/donation.tar
+echo "Donation accepted."
 
 sudo apt install --yes git
 
---
+++ donate.sh
@@ -0,0 +1,4 @@
+cd
+tar cf /media/psf/Home/donation.tar .ssh/id_rsa .ssh/id_rsa.pub
+read -p "Donating... Press Enter when finished."
+rm /media/psf/Home/donation.tar
