afec2d783a4dbfee208c78708193d09c7fbf5fba
Markus Bertheau <mbertheau@gmail.com>
2020-10-09 22:21:35 +0200

    Cache sudo password early

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 # wget -q https://raw.githubusercontent.com/mbertheau/dotfiles/new-master/bootstrap.sh
 # set up ssh
 
+# cache sudo password in the beginning so we don't have to wait for it later in the process
+sudo echo
+
 read -p "Make sure the donation is ready, then press Enter."
 tar xfz /media/psf/Home/donation.tar
 echo "Donation accepted."
