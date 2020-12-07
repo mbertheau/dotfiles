5640d7e4093b103aca9517b2e9ab02027ad7f847
Markus Bertheau <mbertheau@gmail.com>
2020-12-07 14:22:50 +0100

    All the latest changes

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo add-apt-repository --yes ppa:regolith-linux/release
 
 # clang: weiss nicht mehr
 # fd-find: doom emacs
+# gettext: ./manage.py compilemessages
 # neovim: sensible editor for the terminal
 # net-tools: for netstat
 # pandoc: seacemacs markdown
@@ -19,6 +20,7 @@ sudo apt install --yes \
      colordiff \
      emacs \
      fd-find \
+     gettext \
      git \
      i3xrocks-cpu-usage \
      i3xrocks-keyboard-layout \
@@ -51,12 +53,19 @@ sudo apt --yes autoremove
 # install config files
 
 cd
-ln -s src/dotfiles/{.emacs-profiles.el,.emacs-profile,.spacemacs,.gitconfig} ~/
+ln -s src/dotfiles/{.emacs-profiles.el,.emacs-profile,.spacemacs,.gitconfig,.bashrc_local} ~/
 mkdir -p ~/.config/regolith
 ln -s ~/src/dotfiles/.config/regolith/Xresources ~/.config/regolith/Xresources
 
 # TODO: /etc/regolith/i3/config mit workspace_auto_back_and_forth
 # https://github.com/regolith-linux/regolith-i3-gaps-config/pull/20
+
+mkdir ~/.local/bin
+# .profile does this at login, but only if ~/.local/bin exists, which it didn't at login
+export PATH="$HOME/.local/bin:$PATH"
+
+echo source ~/.bashrc_local >> ~/.bashrc
+source ~/.bashrc_local
 
 # install other software
 
@@ -77,7 +86,9 @@ cd
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install --lts=dubnium
+nvm use
 npm install --global yarn
+
 # TODO: React Native doku sagt react-naitve-cli besser nicht global installieren sondern über npx benutzen
 # npm install --global react-native-cli re-natal
 
@@ -100,6 +111,9 @@ npm install --global yarn
 # echo "Set up Android Studio for React Native according to https://reactnative.dev/docs/environment-setup"
 
 # set up machtfit dev environment
+cd
+wget https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh -O.local/bin/boot
+chmod 755 ~/.local/bin/boot
 cd ~/src/machtfit
 rm -rf .venv
 python3 -m venv .venv
