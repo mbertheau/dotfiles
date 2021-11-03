3784dcc9a8065ab842d8684dd386b244456236d9
Markus Bertheau <mbertheau@gmail.com>
2021-11-03 10:20:47 +0100

    Doom: set name, email and which-key delay

--- doom-config/config.el
+++ doom-config/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Markus Bertheau"
+      user-mail-address "mbertheau@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -52,3 +52,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! which-key
+  (setq which-key-idle-delay 0.1))
