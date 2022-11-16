1609c044da23d5b6ec9d2ceabee67c30e22f7beb
Markus Bertheau <mbertheau@gmail.com>
2022-11-16 23:31:26 +0100

    Update path to work org files

--- doom-config/config.el
+++ doom-config/config.el
@@ -30,7 +30,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "/media/psf/Home/OneDrive - machtfit GmbH/org/")
+(setq org-directory "/media/psf/Home/Library/CloudStorage/OneDrive-machtfitGmbH/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
