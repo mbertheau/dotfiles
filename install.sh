199986bedf24ab1fc5dcce7b2a0a90aa4a5ab47b
Markus Bertheau <mbertheau@gmail.com>
2022-11-17 17:51:40 +0100

    Remove nonexisting service

--- install.sh
+++ install.sh
@@ -122,7 +122,6 @@ source ~/.bashrc_local
 systemctl --user mask \
     tracker-extract-3.service \
     tracker-miner-fs-3.service \
-    tracker-miner-rss-3.service \
     tracker-writeback-3.service \
     tracker-xdg-portal-3.service \
     tracker-miner-fs-control-3.service
