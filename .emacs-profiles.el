6236b6861076ed85af7ad80643f7df74d41e32e4
Markus Bertheau <mbertheau@gmail.com>
2022-12-20 09:16:36 +0100

    Add experimental emacs configurations

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,4 @@
 (("doom" . ((user-emacs-directory . "~/src/doom-emacs")))
- ("spacemacs" . ((user-emacs-directory . "~/src/spacemacs"))))
+ ("spacemacs" . ((user-emacs-directory . "~/src/spacemacs")))
+ ("vanilla" . ((user-emacs-directory . "~/src/emacs-vanilla")))
+ ("straight" . ((user-emacs-directory . "~/src/emacs-straight"))))
