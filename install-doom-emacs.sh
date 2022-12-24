59eeed395d5797de6ba67b8b93a254a86ff09569
Markus Bertheau <mbertheau@gmail.com>
2022-12-24 11:54:33 +0100

    Cyberpunk theme

--- install-doom-emacs.sh
+++ install-doom-emacs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.doom.d/themes
+cd ~/.doom.d/themes
+wget https://raw.githubusercontent.com/the-frey/cyberpunk-2019/master/cyberpunk-2019-theme.el
+
 cd ~/src
 rm -rf doom-emacs
 git clone https://github.com/hlissner/doom-emacs
