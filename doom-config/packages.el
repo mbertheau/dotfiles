ee3c6ce152a983a5b65db7ae18f179dfa0fa2a2d
Markus Bertheau <mbertheau@gmail.com>
2022-12-25 16:59:52 +0100

    Add highlight-stages.
    Works well with elisp, not yet at all with ClojureScript. Will have to investigate.

--- doom-config/packages.el
+++ doom-config/packages.el
@@ -51,6 +51,9 @@
 
 (package! rainbow-identifiers)
 ;; (package! color-identifiers-mode)
+(package! highlight-stages)
+;; TODO: make highlight-stages work in clojure-mode
+
 (package! command-log-mode :recipe (:host github :repo "bmag/command-log-mode" :branch "color"))
 (package! magit-delta)
 
