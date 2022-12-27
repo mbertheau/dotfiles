85736c9ba6521169d0dd5fe24f04f26e4d92063a
Markus Bertheau <mbertheau@gmail.com>
2022-12-27 13:40:02 +0100

    Show language keywords (defn, class, ...) without color
    to distinguish them from made-up names that I want to distinguish by
    color.

--- doom-config/custom.el
+++ doom-config/custom.el
@@ -10,4 +10,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-keyword-face ((t (:foreground "white smoke"))))
+ '(font-lock-string-face ((t (:foreground "gray")))))
