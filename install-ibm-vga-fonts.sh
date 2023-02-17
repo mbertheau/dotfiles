45477af844bf592f5d7f1001a830957a22f41017
Markus Bertheau <mbertheau@gmail.com>
2023-02-17 18:20:48 +0100

    fonts

--- install-ibm-vga-fonts.sh
+++ install-ibm-vga-fonts.sh
@@ -1,3 +1,10 @@
-https://int10h.org/oldschool-pc-fonts/download/
+#https://int10h.org/oldschool-pc-fonts/download/
 
-We don't need the Ac fonts. Mx (ttf + bitmap) work very well so far.
+#We don't need the Ac fonts. Mx (ttf + bitmap) work very well so far.
+
+cd ~/Downloads
+mkdir oldschool-pc-font-pack
+cd oldschool-pc-font-pack
+wget https://int10h.org/oldschool-pc-fonts/download/oldschool_pc_font_pack_v2.2_linux.zip
+unzip oldschool_pc_font_pack_v2.2_linux.zip
+cp "ttf - Mx (mixed outline+bitmap)/Mx437_IBM_VGA_9x16.ttf" ~/.local/share/fonts/
--- install.sh
+++ install.sh
@@ -148,6 +148,7 @@ git checkout upstream/develop
 
 
 ~/src/dotfiles/install-doom-emacs.sh
+~/src/dotfiles/install-ibm-vga-fonts.sh
 
 cd
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
