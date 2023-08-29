5fcd6b846e5860b00fd7da58f8600fe95ef04599
Markus Bertheau <markus.bertheau@aiven.io>
2023-08-29 15:22:33 +0200

    Install schemaspy

---
+++ install-schemaspy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt install dot
+mkdir -p ~/schemaspy
+cd ~/schemaspy
+wget https://github.com/schemaspy/schemaspy/releases/download/v6.2.4/schemaspy-6.2.4.jar
+wget https://jdbc.postgresql.org/download/postgresql-42.6.0.jar
--- install.sh
+++ install.sh
@@ -202,3 +202,5 @@ rm linux-install-1.11.1.1224.sh
 
 
 ~/src/dotfiles/install-cljfmt.sh
+
+~/src/dotfiles/install-schemaspy.sh
