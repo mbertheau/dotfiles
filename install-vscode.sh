ce52d809136efc63e8682a0dcfcebc40a44e2f31
Markus Bertheau <markus.bertheau@aiven.io>
2025-04-15 20:35:29 +0200

    Fix vscode install script

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+#https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 
 cd ~/Downloads
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64
-sudo dpgk -i code_1*.deb
+wget --trust-server-names https://code.visualstudio.com/sha/download?build=stable\&os=linux-deb-arm64
+sudo dpkg -i code_1*.deb
