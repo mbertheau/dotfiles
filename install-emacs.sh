a2835238bbfa20cb788143bcbbc1d39c1ac9cd66
Markus Bertheau <markus.bertheau@aiven.io>
2026-08-30 18:55:22 +0200

    Pass --env so doom install does not prompt
    doom install defaults --config and --install to yes, but
    prompts for the envvar file. --env is the yes we give that
    prompt; it skips y-or-n-p and still runs doom sync --env.

--- install-emacs.sh
+++ install-emacs.sh
@@ -6,4 +6,4 @@ git clone https://github.com/plexus/chemacs2.git ~/.config/emacs
 cd ~/src
 rm -rf doom-emacs
 git clone https://github.com/hlissner/doom-emacs
-doom-emacs/bin/doom install
+doom-emacs/bin/doom install --env
